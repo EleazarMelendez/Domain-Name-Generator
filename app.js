2ceaa219f27f9ac7ff9659778bcd8fd8ffe75b01
EleazarMelendez <edavidmelendez@gmail.com>
2023-02-17 19:54:56 +0000

    final project with solution

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-let pronoun = ['the','our', 'one', 'many', ];
-let adj = ['great', 'big', 'old', 'rich', 'blue'];
-let noun = ['jogger','racoon','boat','maserati'];
+let article = ["a", "one", "the"];
+let adjective = ["black", "great", "simple", "worthwile"];
+let noun = ["color", "dog", "car"];
+let domainSuffix = [".com", ".net", ".org", ".miami"];
 
-function generateDomain (a,b,c) {
-    for (let i = 0; i < a.length; i++) {
-        let element1 = a[i];
-        return element1;}
-        for (let j = 0; j < b.length; j++) {
-            let element2 = b[i][j];
-            return element2;
-            }
-            for (let k = 0; k < c.length; k++) {
-                let element3 = c[i][j][k];
-                return element3;
-                }
-        console.log(element1+element2+element3)
-}
+function generateDomain(article, adjective, noun, domainSuffix) {
+  let domains = [];
+  for (let x of article) {
+    for (let y of adjective) {
+      for (let z of noun) {
+        for (let u of domainSuffix) {
+          domains.push(x + y + z + u);
+        }
+      }
+    }
+  }
+  return domains;
+};
 
-generateDomain(pronoun,adj,noun);
+console.log(generateDomain(article, adjective, noun, domainSuffix));
